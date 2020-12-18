d864b3ef2f18c3bf8b5f2dca0b40f3bee6f0e450
Tucker Bendix <58413404+tmb5cg@users.noreply.github.com>
2020-12-17 20:06:19 -0500

    Updates

--- 1.js
+++ 1.js
@@ -2,6 +2,8 @@ function fn1 () {
     alert();
 }
 
+var obj = {};
+
 function myFunction() {
   var txt;
   if (confirm("Press a button!")) {
@@ -10,4 +12,40 @@ function myFunction() {
     txt = "You pressed Cancel!";
   }
   document.getElementById("demo").innerHTML = txt;
+}
+
+
+function getCalendarIDs() {
+  return gapi.client.calendar.calendarList.list({})
+      .then(function(response) {
+              // Handle the results here (response.result has the parsed body).
+              console.log("Response", response);
+
+              x = response.items;
+              test = x[0].id;
+              console.log("YEEEET", test)
+              // obj = JSON.parse(response)
+              // console.log("Response2", obj)
+              //
+              // entries = obj.items
+
+            //   console.log("Yeet", response.items)
+              // Assign result to obj
+             // obj = JSON.parse(response)
+             //
+             //  jsonresults = obj.items;
+             //  console.log("Response", obj)
+              // test = jsonresults[0].id;
+              // document.getElementById("checkbox1").innerHTML = test;
+              //
+              //
+              // for (var i=0; i<jsonresults.length; i++) {
+              //   for (var name in jsonresults[i]) {
+              //       console.log(i)
+              //         console.log(name)
+              //     }
+              // }
+
+            },
+            function(err) { console.error("Execute error", err); });
 }
